76b7ccb0bdf7424f403d310dda6f8e2e60adffca
samer-mansouri <samer.mansouri@insat.ucar.tn>
2025-01-26 20:03:29 +0100

    fixing permissions for some views

--- backend/urls.py
+++ backend/urls.py
@@ -27,7 +27,4 @@ urlpatterns = [
     path('api/product/', include('product.urls')),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
