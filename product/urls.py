94cb57a4d6b7b6d06bca191b9d65711816484ad7
samer-mansouri <samer.mansouri@insat.ucar.tn>
2025-01-29 20:27:17 +0100

    feat: pagination and search blog and products

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import ProductListView, ProductCreateView, ProductDetailView, ProductUpdateView, ProductDeleteView, CreateCheckoutSessionView, PaymentSuccessView, UserOrdersView, CheckoutSpecificOrderView
+from .views import ProductListView, ProductCreateView, ProductDetailView, ProductUpdateView, ProductDeleteView, CreateCheckoutSessionView, PaymentSuccessView, UserOrdersView, CheckoutSpecificOrderView, PaginatedAndSearchProductListView
 
 app_name = 'product'
 urlpatterns = [
@@ -13,4 +13,5 @@ urlpatterns = [
     path('payment-success/', PaymentSuccessView.as_view(), name='payment_success'),
     path('user-orders/', UserOrdersView.as_view(), name='user_orders'),  # New endpoint to get user orders
     path('checkout-specific-order/', CheckoutSpecificOrderView.as_view(), name='checkout_specific_order'),  # New endpoint to checkout a specific order
+    path('paginated-search/', PaginatedAndSearchProductListView.as_view(), name='paginated_search'),  # New endpoint to search and paginate products
 ]
