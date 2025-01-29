2f3bb7a70abc45803c16be29a58a0157661f5946
samer-mansouri <samer.mansouri@insat.ucar.tn>
2025-01-29 20:09:49 +0100

    feat: pay specefic order

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import ProductListView, ProductCreateView, ProductDetailView, ProductUpdateView, ProductDeleteView, CreateCheckoutSessionView, PaymentSuccessView, UserOrdersView
+from .views import ProductListView, ProductCreateView, ProductDetailView, ProductUpdateView, ProductDeleteView, CreateCheckoutSessionView, PaymentSuccessView, UserOrdersView, CheckoutSpecificOrderView
 
 app_name = 'product'
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create_checkout_session'),
     path('payment-success/', PaymentSuccessView.as_view(), name='payment_success'),
     path('user-orders/', UserOrdersView.as_view(), name='user_orders'),  # New endpoint to get user orders
+    path('checkout-specific-order/', CheckoutSpecificOrderView.as_view(), name='checkout_specific_order'),  # New endpoint to checkout a specific order
 ]
